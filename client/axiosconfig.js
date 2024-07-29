f4c330fd8179ebef3211f15965cbe5ed8e4e53f3
Rishi <rishimaheshwari040@gmail.com>
2024-07-29 13:03:32 +0530

    update

--- client/axiosconfig.js
+++ client/axiosconfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: "https://api.eagleart.in",
-    baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
+    baseURL: "https://id-card-y8ui.onrender.com",
     withCredentials: true,
 });
 
