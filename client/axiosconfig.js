dbf8e758faee60b0bbc852a6a5901062526e6d0f
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-08-08 12:46:44 +0530

    update

--- client/axiosconfig.js
+++ client/axiosconfig.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL: "https://api.eagleart.in",
     // baseURL: "http://localhost:8080",
-    baseURL: "https://id-card-y8ui.onrender.com",
+    baseURL: "https://testid.mahitechnocrafts.in",
+    // baseURL: "https://id-card-y8ui.onrender.com",
     withCredentials: true,
 });
 
